17af298422a181ca596e7b3bd19ea87eb92444b4
devlipe <felipe.p.ferreira@ufv.br>
2024-11-20 19:00:23 -0300

    fix the cancel exit when esc is pressed

--- src/app/app_context.rs
+++ src/app/app_context.rs
@@ -1,21 +1,20 @@
 use crate::{Config, DBConnector, Output, SQLiteConnector, VariablesConfig};
 use identity_iota::storage::Storage;
 use identity_stronghold::StrongholdStorage;
-use iota_sdk::client::{Client, Password};
 use iota_sdk::client::secret::stronghold::StrongholdSecretManager;
+use iota_sdk::client::{Client, Password};
 use tokio::sync::watch;
 
 pub struct AppContext {
     pub tangle_client: Client,
     pub db: Box<dyn DBConnector>,
     pub stronghold_storage: StrongholdStorage,
-    pub storage : Storage<StrongholdStorage, StrongholdStorage>,
+    pub storage: Storage<StrongholdStorage, StrongholdStorage>,
 }
 
 impl AppContext {
-
     pub async fn build_app_context_with_loading() -> Self {
-        let (tx , rx)= watch::channel(true);
+        let (tx, rx) = watch::channel(true);
         // Spawn the loading animation as a background task
         let animation_handle = tokio::spawn(Output::loading_animation(rx));
 
@@ -26,53 +25,57 @@ impl AppContext {
 
         // Wait for the animation task to finish
         animation_handle.await.unwrap();
-        
+
         context
     }
-    
-    pub async fn my_app_context() -> Self {
 
+    pub async fn my_app_context() -> Self {
         let config = VariablesConfig::get();
         let tangle_client = AppContext::get_tangle_client(config).await.unwrap();
         let db = AppContext::get_sqlite_database(config);
         let stronghold_storage = AppContext::get_stronghold_storage(config);
         let storage = Storage::new(stronghold_storage.clone(), stronghold_storage.clone());
-        
+
         AppContext {
             tangle_client,
             db,
             stronghold_storage,
             storage,
         }
-        
     }
-    
-    fn get_sqlite_database(config : &dyn Config) -> Box<dyn DBConnector> {
-        let sqlite_path : &String = config.get_value("sqlite_path");
-        let sqlite =  SQLiteConnector::new(sqlite_path).unwrap_or_default();
-        
+
+    fn get_sqlite_database(config: &dyn Config) -> Box<dyn DBConnector> {
+        let sqlite_path: &String = config.get_value("sqlite_path");
+        let sqlite = SQLiteConnector::new(sqlite_path).unwrap_or_default();
+
         Box::new(sqlite)
     }
-    async fn get_tangle_client(config : &dyn Config) -> anyhow::Result<Client > {
+    async fn get_tangle_client(config: &dyn Config) -> anyhow::Result<Client> {
         let client: Client = Client::builder()
             .with_primary_node(config.get_value("api_endpoint"), None)?
             .finish()
             .await?;
         Ok(client)
     }
-    
-    fn get_stronghold_storage(config : &dyn Config) -> StrongholdStorage {
+
+    fn get_stronghold_storage(config: &dyn Config) -> StrongholdStorage {
         // Stronghold password.
         let password = Password::from(config.get_value("stronghold_password").to_owned());
         // Stronghold snapshot path.
         let stronghold_path: std::path::PathBuf =
-        std::path::PathBuf::from(config.get_value("stronghold_path").to_owned());
+            std::path::PathBuf::from(config.get_value("stronghold_path").to_owned());
+
+        let mut start = std::time::Instant::now();
 
         let stronghold = StrongholdSecretManager::builder()
             .password(password.clone())
             .build(stronghold_path.clone()).unwrap();
 
+        println!("Time to get stronghold: {:?}", start.elapsed());
+        start = std::time::Instant::now();
         let stronghold_storage = StrongholdStorage::new(stronghold);
+        println!("Time to get stronghold storage: {:?}", start.elapsed());
+
         stronghold_storage
     }
 }
--- src/command/exit_app_command.rs
+++ src/command/exit_app_command.rs
@@ -1,15 +1,19 @@
+use crate::{AppContext, Command, Output, ScreenEvent};
 use std::io;
+use std::io::stdin;
 use termion::event::Key;
 use termion::input::TermRead;
-use crate::{AppContext, Command, Output, ScreenEvent};
+use termion::raw::IntoRawMode;
 
 pub struct  ExitAppCommand;
 
 impl Command for ExitAppCommand {
     fn execute(&mut self, _context: &AppContext) -> ScreenEvent {
         self.print_tile();
-        ExitAppCommand::show_exit_message();
-        ScreenEvent::Exit
+        let event = ExitAppCommand::show_exit_message();
+        // Return the event
+        println!("ExitAppCommand: {:?}", event);
+        event
     }
 
     fn print_tile(&self) {
@@ -19,25 +23,30 @@ impl Command for ExitAppCommand {
 }
 
 impl  ExitAppCommand {
-    fn show_exit_message() {
+    fn show_exit_message() -> ScreenEvent {
         println!("It is a shame that we have to part ways. Goodbye!");
         println!("Press any key to exit (ESC to cancel):");
 
-        // Create a stdin lock to read key events
-        let stdin = io::stdin();
+        let stdin = stdin();
+        let mut keys = stdin.keys();
 
-        // Loop to handle key presses
-        for key in stdin.keys() {
-            match key.unwrap() {
+        // Enable raw mode
+        let _stdout = io::stdout().into_raw_mode().unwrap();
+
+        while let Some(Ok(key)) = keys.next() {
+            return match key {
                 Key::Esc => {
                     println!("Exit cancelled");
-                    return; // Cancel exit if ESC is pressed
+                    ScreenEvent::Cancel // Cancel exit if ESC is pressed
                 }
                 _ => {
                     println!("Exiting...");
-                    break; // Exit on any other key press
+                    ScreenEvent::Exit // Exit if any other key is pressed
                 }
             }
         }
+
+        // Default case
+        ScreenEvent::Exit
     }
 }
